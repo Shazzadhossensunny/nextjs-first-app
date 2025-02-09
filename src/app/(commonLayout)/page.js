217dd1c7b2375d6818597fc06c0f9c5d4d953d04
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-02-10 02:53:21 +0600

    revalidate add fo ssr

--- src/app/(commonLayout)/page.js
+++ src/app/(commonLayout)/page.js
@@ -2,13 +2,17 @@ import ProductCard from "@/components/ProductCard";
 import React from "react";
 
 export default async function HomePage() {
-  const res = await fetch("http://localhost:5000/products");
+  const res = await fetch("http://localhost:5000/products", {
+    next: {
+      revalidate: 30,
+    },
+  });
   const products = await res.json();
   console.log(products);
   return (
     <div className="container mx-auto">
       <h1 className="text-center text-4xl font-semibold mb-10">
-        Features Product Fetching and Caching with Next Js
+        Features Product Fetching, Caching and Revalidating with Next Js
       </h1>
       <div className="grid grid-cols-3 gap-6">
         {products.map((product) => (
